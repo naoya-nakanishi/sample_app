de056f651712584849c3257dfe2882851d4109dd
Naoya-tech <naoya.techwork@gmail.com>
2019-09-24 19:22:33 +0900

    pagetitleの最適化

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,14 +2,15 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get root" do
-    get FILL_IN
-    assert_response FILL_IN
-  end
+  # test "should get root" do
+  #   get FILL_IN
+  #   assert_response FILL_IN
+  # end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
 
